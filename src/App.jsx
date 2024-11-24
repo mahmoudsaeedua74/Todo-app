f4248d0d11615199bb7468291433d95e0f2d3442
mahmoudsaeedua74 <mahmoudsaeed0112074@gmail.com>
2024-11-24 13:50:46 +0200

    change form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
+
 import { useState } from 'react'
 import './App.css'
-import Logo from './componat/Logo/Logo';
 import Form from './componat/Form/Form';
 import PackingList from './componat/PackingList/PackingList';
 import Usercontext from './Context/Usercontext';
@@ -9,11 +9,11 @@ function App() {
 
   return (
     <>
-      <Usercontext>
-        <Logo />
-        <Form />
-        <PackingList />
-      </Usercontext>
+
+        <Usercontext> 
+          <Form />
+          <PackingList />
+        </Usercontext>
 
     </>
   )
